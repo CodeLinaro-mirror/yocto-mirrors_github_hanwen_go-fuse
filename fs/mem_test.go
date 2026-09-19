73d807971a6299af2343be71d1a0f95f1f9402e7
Han-Wen Nienhuys <hanwenn@gmail.com>
2026-09-19 16:14:22 +0200

    fs: consider panics as test failures too
    Change-Id: I9ba2d2fdeac191eb9c2d5b8513b3f0456a6a6964

--- fs/mem_test.go
+++ fs/mem_test.go
@@ -8,9 +8,11 @@ import (
 	"bytes"
 	"context"
 	"fmt"
+	"log"
 	"math/rand"
 	"os"
 	"reflect"
+	"runtime"
 	"sync"
 	"syscall"
 	"testing"
@@ -31,6 +33,13 @@ func testMount(t *testing.T, root InodeEmbedder, opts *Options) (string, *fuse.S
 		}
 	}
 	opts.Debug = testutil.VerboseTest()
+	opts.PanicHandler = func(e any) fuse.Status {
+		buf := make([]byte, 64<<10)
+		buf = buf[:runtime.Stack(buf, false)]
+		log.Printf("panic in FS handler: %v\n%s", e, buf)
+		t.Fail()
+		return fuse.EIO
+	}
 
 	server, err := Mount(mntDir, root, opts)
 	if err != nil {
